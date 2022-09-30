dc0a098aed2e5f7f484b8e325d6d229249c85697
David Ankin <daveankin@gmail.com>
2022-09-30 14:02:34 -0400

    changes so far

--- spring/spring-basics/src/main/java/info/ankin/experiments/prometheus/spring/basics/BasicsController.java
+++ spring/spring-basics/src/main/java/info/ankin/experiments/prometheus/spring/basics/BasicsController.java
@@ -10,15 +10,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+import java.util.Random;
 
 @Controller
 public class BasicsController {
+    Random random = new Random();
     State state = new State();
 
     @Counted(value = "home")
     @RequestMapping
     Mono<ResponseEntity<State>> get() {
         return Mono.delay(Duration.ofMillis(state.sleep)).then(Mono.just(ResponseEntity.ok(state)));
+    }
+
+    @Counted(value = "random")
+    @RequestMapping("/random")
+    Mono<ResponseEntity<State>> random() {
+        return Mono.delay(Duration.ofMillis(random.nextInt(state.getSleep()))).then(Mono.just(ResponseEntity.ok(state)));
     }
 
     @RequestMapping(method = RequestMethod.POST)
