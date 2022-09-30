a8203899dc029e88dc61e7bce5d0b0fe12581131
David Ankin <daveankin@gmail.com>
2022-09-30 12:41:58 -0400

    basic spring boot stats

--- spring/spring-basics/src/main/java/info/ankin/experiments/prometheus/spring/basics/BasicsApplication.java
+++ spring/spring-basics/src/main/java/info/ankin/experiments/prometheus/spring/basics/BasicsApplication.java
@@ -2,10 +2,12 @@ package info.ankin.experiments.prometheus.spring.basics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.scheduler.Schedulers;
 
 @SpringBootApplication
 public class BasicsApplication {
     public static void main(String[] args) {
+        Schedulers.enableMetrics();
         SpringApplication.run(BasicsApplication.class, args);
     }
 }
